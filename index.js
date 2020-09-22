d97b3fe132c954137f63f2f956ec238e8467d1ed
Brian <brian@perlego.com>
2020-09-22 11:40:33 -0400

    First test of rsa encryption/decryption

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { NativeModules } from 'react-native';
 
-const { Crypto } = NativeModules;
+const { Crypto, RSA } = NativeModules;
 
 export const encryptAES256CBC = (clearText, key, iv) => {
   return Crypto.encryptAES256CBC(clearText, key, iv);
@@ -8,6 +8,14 @@ export const encryptAES256CBC = (clearText, key, iv) => {
 
 export const decryptAES256CBC = (cipherText, key, iv, base64) => {
   return Crypto.decryptAES256CBC(cipherText, key, iv, base64);
+};
+
+export const encryptRSA = (str, pubKey) => {
+  return RSA.encryptRSA(str, pubKey);
+};
+
+export const decryptRSA = (str, privateKey) => {
+  return RSA.decryptRSA(str, privateKey);
 };
 
 export const encodeBase64 = (text) => {
